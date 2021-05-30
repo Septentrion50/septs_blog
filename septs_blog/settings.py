f0717e73fc1bdf8b52db6781b75d8c7a287d1bd6
Septentrion50 <erwan56lecorre@gmail.com>
2021-05-31 01:53:14 +0200

    Ajout d'un gitignore et du module python-dotenv

--- septs_blog/settings.py
+++ septs_blog/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-import secrets
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ PROJECT_DIR = os.path.join(BASE_DIR, "septs_blog")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrets.secret_key()
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
